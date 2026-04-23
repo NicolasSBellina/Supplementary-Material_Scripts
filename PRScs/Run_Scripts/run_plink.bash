4e6d315897415454fd364c4ad0785fe371539a1f
Nicolas Scheffer Bellina <nicolas.bellina@ufcspa.edu.br>
2026-04-23 12:24:03 -0300

    Add files via upload

---
+++ PRScs/Run_Scripts/run_plink.bash
@@ -0,0 +1,43 @@
+#./plink --bfile nome --score nome_do_novo_arquivo.txt 2 4 6 sum --out nome_do_arquivo_PRS
+
+#./plink --bfile /mnt/c/users/nicolas/desktop/prs/amostra_alvo/crianças/BANCO_FILTER_ADHD_QC --score /mnt/c/users/Nicolas/desktop/prs/results/crianças/scores/IM/IM_pst_eff_a1_b0.5_phiauto_chr_all.txt 2 4 6 --out /mnt/c/users/Nicolas/desktop/prs/results/crianças/profiles/IM/IM
+
+#DIR="/mnt/c/users/Nicolas/desktop/prs/results/crianças/scores"
+DIR="/mnt/c/users/Nicolas/desktop/prs/results/adultos/scores"
+PLINK="/mnt/c/users/Nicolas/desktop/prs/plink"
+#OUTDIR="/mnt/c/users/Nicolas/desktop/prs/results/crianças/profiles"
+OUTDIR="/mnt/c/users/Nicolas/desktop/prs/results/adultos/profiles"
+#BFILE="/mnt/c/Users/Nicolas/Desktop/prs/amostra_alvo/crianças/BANCO_FILTER_ADHD_QC"
+BFILE="/mnt/c/Users/Nicolas/Desktop/prs/amostra_alvo/adultos/PSY_GSA1-GSA2_merged_geno0.05_maf0.01_mind0.05_hwe1e-6-FINAL-hg19rsID-bravo-dbSNP155-rm-dup-b"
+
+traits=(
+IM
+CAD
+FA
+Hip
+IC
+Stroke
+)
+
+files=(
+IM_pst_eff_a1_b0.5_phiauto_chr_all.txt
+CAD_pst_eff_a1_b0.5_phiauto_chr_all.txt
+FA_pst_eff_a1_b0.5_phiauto_chr_all.txt
+Hip_pst_eff_a1_b0.5_phiauto_chr_all.txt
+IC_pst_eff_a1_b0.5_phiauto_chr_all.txt
+Stroke_pst_eff_a1_b0.5_phiauto_chr_all.txt
+)
+
+
+for i in "${!traits[@]}"
+do
+    mkdir -p $OUTDIR/${traits[$i]}
+
+    echo "****************************************** ${traits[$i]} **************************************************************"
+
+    "$PLINK" \
+    --bfile "$BFILE" \
+    --score "$DIR/${traits[$i]}/${files[$i]}" 2 4 6 \
+    --out "$OUTDIR/${traits[$i]}/${traits[$i]}"
+
+done
---
+++ PRScs/Run_Scripts/run_prs.bash
@@ -0,0 +1,51 @@
+cd /mnt/c/users/Nicolas/Desktop/prs/sum
+
+#DIR="/mnt/c/users/Nicolas/Desktop/prs/sum/"
+PRS="/mnt/c/users/Nicolas/Desktop/prs/PRScs/PRScs.py"
+REF="/mnt/c/Users/Nicolas/Desktop/prs/PRSref/ldblk_1kg_eur"
+#BIM="/mnt/c/Users/Nicolas/Desktop/prs/amostra_alvo/crianças/BANCO_FILTER_ADHD_QC"
+BIM="/mnt/c/Users/Nicolas/Desktop/prs/amostra_alvo/adultos/PSY_GSA1-GSA2_merged_geno0.05_maf0.01_mind0.05_hwe1e-6-FINAL-hg19rsID-bravo-dbSNP155-rm-dup-b"
+#OUTDIR="/mnt/c/users/Nicolas/Desktop/prs/results/crianças/scores"
+OUTDIR="/mnt/c/users/Nicolas/Desktop/prs/results/adultos/scores"
+
+traits=(
+IM
+CAD
+FA
+Hip
+IC
+Stroke
+)
+
+files=(
+IM_PRS_SE.txt
+CAD_PRS_SE.txt
+FA_PRS_SE.txt
+Hip_PRS_SE.txt
+IC_PRS_SE.txt
+Stroke_PRS_SE.txt
+)
+
+n=(
+639221
+1165690
+1030836
+458554
+1847875
+1308460
+)
+
+for i in "${!traits[@]}"
+do
+    mkdir -p $OUTDIR/${traits[$i]}
+
+    echo "******************************************Calculando PRS para ${traits[$i]} **************************************************************"
+
+    python "$PRS" \
+    --ref_dir "$REF" \
+    --bim_prefix "$BIM" \
+    --sst_file "${files[$i]}" \
+    --n_gwas "${n[$i]}" \
+    --out_dir "$OUTDIR/${traits[$i]}/${traits[$i]}"
+
+done
