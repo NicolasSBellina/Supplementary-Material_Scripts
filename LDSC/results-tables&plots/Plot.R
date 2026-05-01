0786f7bf77c0647950696bb5f70233138289cfe8
Nicolas Scheffer Bellina <nicolas.bellina@ufcspa.edu.br>
2026-05-01 18:53:12 -0300

    Update Plot.R

--- LDSC/results-tables&plots/Plot.R
+++ LDSC/results-tables&plots/Plot.R
@@ -2,51 +2,118 @@ library(tidyr)
 library(dplyr)
 library(corrplot)
 
+setwd("C:/Users/Nicolas/Desktop/sum_hm3_ldsc/results/qualitativo/")
+
+png("temp2.png", width=10*400, height=5*400, res=400)
+par(mfrow=c(1,2), mar=c(4, 4, 2, 2))
+
 # read table
-df <- read.table("C:/Users/Nicolas/Desktop/sum_hm3_ldsc/results/qualitativo/rg_long.tsv", header=TRUE, sep="\t")
+ADHD_Diag <- read.table("C:/Users/Nicolas/Desktop/sum_hm3_ldsc/results/qualitativo/rg_long.tsv", header=TRUE, sep="\t")
+ADHD_Symp <-  read.table("C:/Users/Nicolas/Desktop/sum_hm3_ldsc/results/quantitativo/rg_long.tsv", header=TRUE, sep="\t")
 
-df$p1 <- ifelse(df$p1 == "IM","MI",df$p1)
-df$p2 <- ifelse(df$p2 == "IM","MI",df$p2)
+ADHD_Diag$p1 <- ifelse(ADHD_Diag$p1 == "IM","MI",ADHD_Diag$p1)
+ADHD_Diag$p2 <- ifelse(ADHD_Diag$p2 == "IM","MI",ADHD_Diag$p2)
 
-df$p1 <- ifelse(df$p1 == "Hip","Hypertension",df$p1)
-df$p2 <- ifelse(df$p2 == "Hip","Hypertension",df$p2)
+ADHD_Diag$p1 <- ifelse(ADHD_Diag$p1 == "Hip","Hypertension",ADHD_Diag$p1)
+ADHD_Diag$p2 <- ifelse(ADHD_Diag$p2 == "Hip","Hypertension",ADHD_Diag$p2)
 
-df$p1 <- ifelse(df$p1 == "FA","AF",df$p1)
-df$p2 <- ifelse(df$p2 == "FA","AF",df$p2)
+ADHD_Diag$p1 <- ifelse(ADHD_Diag$p1 == "FA","AF",ADHD_Diag$p1)
+ADHD_Diag$p2 <- ifelse(ADHD_Diag$p2 == "FA","AF",ADHD_Diag$p2)
 
-df$p1 <- ifelse(df$p1 == "FC","HR",df$p1)
-df$p2 <- ifelse(df$p2 == "FC","HR",df$p2)
+ADHD_Diag$p1 <- ifelse(ADHD_Diag$p1 == "FC","HR",ADHD_Diag$p1)
+ADHD_Diag$p2 <- ifelse(ADHD_Diag$p2 == "FC","HR",ADHD_Diag$p2)
 
-df$p1 <- ifelse(df$p1 == "IC","HF",df$p1)
-df$p2 <- ifelse(df$p2 == "IC","HF",df$p2)
+ADHD_Diag$p1 <- ifelse(ADHD_Diag$p1 == "IC","HF",ADHD_Diag$p1)
+ADHD_Diag$p2 <- ifelse(ADHD_Diag$p2 == "IC","HF",ADHD_Diag$p2)
 
-tdah <- "ADHD"
+tdah <- "ADHD_Diag"
 
 # >>> EDIT HERE according to your phenotypes
-qualitativos <- c("CAD", "MI", "HF", "AF", "Stroke", "DCM", "Hypertension")
+qualitativos <- c("HF", "Stroke", "MI", "CAD", "Hypertension", "AF", "DCM")
 quantitativos <- c("SBP", "DBP", "PP", "HR")
 
-rg_qual <- df %>%
-  filter((p1 == tdah & p2 %in% qualitativos) |
-           (p2 == tdah & p1 %in% qualitativos)) %>%
-  mutate(trait = ifelse(p1 == tdah, p2, p1)) %>%
-  arrange(desc(rg))
 
-ord_qual <- rg_qual$trait
-
-rg_quant <- df %>%
-  filter((p1 == tdah & p2 %in% quantitativos) |
-           (p2 == tdah & p1 %in% quantitativos)) %>%
-  mutate(trait = ifelse(p1 == tdah, p2, p1)) %>%
-  arrange(desc(rg))
-
-ord_quant <- rg_quant$trait
-
-traits <- c(tdah, ord_qual, ord_quant)
+traits <- c(tdah, qualitativos, quantitativos)
 
 # force order as factor (important)
-df$p1 <- factor(df$p1, levels = traits)
-df$p2 <- factor(df$p2, levels = traits)
+ADHD_Diag$p1 <- factor(ADHD_Diag$p1, levels = traits)
+ADHD_Diag$p2 <- factor(ADHD_Diag$p2, levels = traits)
+
+# rg mat_Diagrix
+mat_Diag <- matrix(NA, length(traits), length(traits))
+rownames(mat_Diag) <- traits
+colnames(mat_Diag) <- traits
+
+# p-value mat_Diagrix
+p_mat_Diag <- matrix(NA, length(traits), length(traits))
+rownames(p_mat_Diag) <- traits
+colnames(p_mat_Diag) <- traits
+
+# fill mat_Diagrices
+for(i in 1:nrow(ADHD_Diag)){
+  
+  t1 <- ADHD_Diag$p1[i]
+  t2 <- ADHD_Diag$p2[i]
+  
+  r <- as.numeric(ADHD_Diag$rg[i])
+  p <- as.numeric(ADHD_Diag$p[i])
+  
+  mat_Diag[t1,t2] <- r
+  mat_Diag[t2,t1] <- r
+  
+  p_mat_Diag[t1,t2] <- p
+  p_mat_Diag[t2,t1] <- p
+}
+
+# diagonal
+diag(mat_Diag) <- 1
+diag(p_mat_Diag) <- 0
+
+# replace NA
+mat_Diag[is.na(mat_Diag)] <- 0
+p_mat_Diag[is.na(p_mat_Diag)] <- 1
+
+# plot
+corrplot(mat_Diag,
+         method="color",
+         type="full",
+         tl.col="black",
+         tl.srt=45,
+         col=colorRampPalette(c("blue","white","red"))(200),
+         p.mat=p_mat_Diag,
+         sig.level=0.00454,
+         insig="label_sig",
+         pch.cex=1.3,
+         pch.col="black")
+
+legend("bottom",
+       legend=c("A"),
+       bty="n",
+       xpd = TRUE,
+       cex = 1.1)
+
+tdah <- "ADHD_Symp"
+
+traits <- c(tdah, qualitativos, quantitativos)
+
+ADHD_Symp$p1 <- ifelse(ADHD_Symp$p1 == "IM","MI",ADHD_Symp$p1)
+ADHD_Symp$p2 <- ifelse(ADHD_Symp$p2 == "IM","MI",ADHD_Symp$p2)
+
+ADHD_Symp$p1 <- ifelse(ADHD_Symp$p1 == "Hip","Hypertension",ADHD_Symp$p1)
+ADHD_Symp$p2 <- ifelse(ADHD_Symp$p2 == "Hip","Hypertension",ADHD_Symp$p2)
+
+ADHD_Symp$p1 <- ifelse(ADHD_Symp$p1 == "FA","AF",ADHD_Symp$p1)
+ADHD_Symp$p2 <- ifelse(ADHD_Symp$p2 == "FA","AF",ADHD_Symp$p2)
+
+ADHD_Symp$p1 <- ifelse(ADHD_Symp$p1 == "FC","HR",ADHD_Symp$p1)
+ADHD_Symp$p2 <- ifelse(ADHD_Symp$p2 == "FC","HR",ADHD_Symp$p2)
+
+ADHD_Symp$p1 <- ifelse(ADHD_Symp$p1 == "IC","HF",ADHD_Symp$p1)
+ADHD_Symp$p2 <- ifelse(ADHD_Symp$p2 == "IC","HF",ADHD_Symp$p2)
+
+# force order as factor (important)
+ADHD_Symp$p1 <- factor(ADHD_Symp$p1, levels = traits)
+ADHD_Symp$p2 <- factor(ADHD_Symp$p2, levels = traits)
 
 # rg matrix
 mat <- matrix(NA, length(traits), length(traits))
@@ -59,13 +126,13 @@ rownames(p_mat) <- traits
 colnames(p_mat) <- traits
 
 # fill matrices
-for(i in 1:nrow(df)){
+for(i in 1:nrow(ADHD_Symp)){
   
-  t1 <- df$p1[i]
-  t2 <- df$p2[i]
+  t1 <- ADHD_Symp$p1[i]
+  t2 <- ADHD_Symp$p2[i]
   
-  r <- as.numeric(df$rg[i])
-  p <- as.numeric(df$p[i])
+  r <- as.numeric(ADHD_Symp$rg[i])
+  p <- as.numeric(ADHD_Symp$p[i])
   
   mat[t1,t2] <- r
   mat[t2,t1] <- r
@@ -95,10 +162,11 @@ corrplot(mat,
          pch.cex=1.3,
          pch.col="black")
 
-legend("right",
-       legend=c("* p < 0.00454"),
+legend("bottom",
+       legend=c("B"),
        bty="n",
-       xpd = TRUE,         # allows drawing outside the plot
+       xpd = TRUE,
        cex = 1.1)
 
+dev.off()
 # Plot package link: https://cran.r-project.org/web/packages/corrplot/vignettes/corrplot-intro.html
